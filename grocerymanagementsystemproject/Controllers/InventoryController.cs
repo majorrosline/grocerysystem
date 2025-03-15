7d600efc86069a61b48bf4e6cee9b866a22c1e9a
majorrosline <majorrosline@gmail.com>
2025-03-15 20:27:18 +0000

    Add files via upload

---
+++ grocerymanagementsystemproject/Controllers/InventoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroceryManagement1.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private GroceryDbContext db = new GroceryDbContext();
+
+        // GET: Inventory
+
+        public ActionResult Index()
+        {
+            return View(db.Inventories.ToList());
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            var item = db.Inventories.Find(id);
+            if (item != null && item.Stock > 0)
+            {
+                Session["Cart"] = item; // Simplified for demonstration
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ grocerymanagementsystemproject/Controllers/OrderController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GroceryManagement1.Models;
+using GroceryManagement1.Services;
+using GroceryManagement1.Commands;
+
+namespace GroceryManagement1.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        private GroceryDbContext db = new GroceryDbContext();
+        private readonly PaymentService _paymentService;
+        private readonly CommandInvoker _commandInvoker;
+
+        public OrderController()
+        {
+            _paymentService = new PaymentService();
+            _commandInvoker = new CommandInvoker();
+        }
+        //[HttpGet]
+        //public ActionResult PlaceOrder()
+        //{
+        //    var userId = (int)Session["UserId"];
+        //    var isPremium = (bool)Session["IsPremium"];
+        //    var cart = (Inventory)Session["Cart"];
+
+        //    if (cart == null) return RedirectToAction("Index", "Inventory");
+
+        //    decimal discount = isPremium ? 0.05m : 0;
+        //    decimal total = cart.Price - (cart.Price * discount);
+
+        //    var order = new Order
+        //    {
+        //        UserId = userId,
+        //        Status = "Pending",
+        //        TotalAmount = total
+        //    };
+
+        //    db.Orders.Add(order);
+        //    db.SaveChanges();
+
+        //    return View(order);
+        //}
+        //[HttpPost]
+        
+
+        //public ActionResult OrderConfirmation(int orderId)
+        //{
+        //    var order = /* fetch order by orderId */;
+        //    return View(order);
+        //}
+    }
+}
